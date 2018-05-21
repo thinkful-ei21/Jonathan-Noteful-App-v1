cb1f8448768240bb86a1f7f60a9517e736fb4318
BuddhaL1T3 <jmriggs75@gmail.com>
2018-05-21 17:44:28 -0400

    completed day one's tasks

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 'use strict';
 
-// Load array of notes
-const data = require('./db/notes');
-
-console.log('Hello Noteful!');
-
 // INSERT EXPRESS APP CODE HERE...
+const express = require('express');
+const data = require('./db/notes');
+const app = express();
+
+// ADD STATIC SERVER HERE
+app.use(express.static('public'));
+
+
+app.get('/api/notes', (req , res) => {
+    if(req.query.searchTerm) {
+        let search = req.query.searchTerm.toLowerCase();
+        res.json(data.filter(item => item.title.toLowerCase().includes(search)||item.content.toLowerCase().includes(search))); 
+    }
+    res.json(data);
+});
+
+app.get('/api/notes/:id', (req , res) => {
+    res.json(data.find(item => item.id === Number(req.params.id)));
+});
+
+
+app.listen(8080, function() {
+    console.info(`server listening on ${this.address().port}`);
+}).on('error', err => {
+    console.error(err);
+});
