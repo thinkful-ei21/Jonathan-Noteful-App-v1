2dbfdae94fca0c31cc509f27bf46d6c17bd905d1
BuddhaL1T3 <jmriggs75@gmail.com>
2018-05-22 16:44:14 -0400

    Add Database and Update Endpoints

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 // INSERT EXPRESS APP CODE HERE...
 const express = require('express');
 const data = require('./db/notes');
+const simDB = require('./db/simDB');
+const notes = simDB.initialize(data);
 const {PORT} = require('./config');
 const {logger} = require('./middleware/logger')
 const app = express();
